020ba2741b66e32d8516dec70e6ac40b2713d391
santi <santi@proper-code.com>
2020-04-29 00:06:55 -0700

    menu

--- app/js/Poster.js
+++ app/js/Poster.js
@@ -2,10 +2,17 @@ class Poster{
     constructor(){
         this.node = document.getElementById( 'posterPreview' )
         this.inner = this.node.querySelector( '.inner' )
+        console.log( this.inner.innerHTML.split( ' ' ) )
+        this.instances = document.querySelectorAll( 'svg.poster' )
+        this.instances.forEach( p => {
+            p.setAttribute( 'width', this.node.offsetWidth )
+            p.setAttribute( 'height', this.node.offsetHeight )
+        })
     }
 
     updateCopy( copy ){
         this.inner.innerHTML = copy
+        
     }
 
     updateSize( size ){
--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,9 @@ import Pages from './Pages/*.js'
 import Page from './Page'
 import Poster from './Poster'
 
+var header = document.querySelector( '#header' )
+var footer = document.querySelector( '#footer' )
+
 class Flow{ 
     constructor( page = 0 ){
         this.node = document.querySelector( '#mainFlow' )
@@ -17,7 +20,7 @@ class Flow{
             this.pages.push( pageObject )
         })
 
-        this.navigate( 0 )
+        this.navigate( 9 )
     }
 
     update( e ){
@@ -44,7 +47,7 @@ class Flow{
     navigate( page ){
         this.page = Math.max( 0, Math.min( page, this.pages.length - 1 ) )
         
-        var footer = document.querySelector( '#footer' )
+        
         if( this.page == 0 ) footer.dataset.navitype = 'first'
         if( this.page > 0 && this.page < this.pages.length - 1 ) footer.dataset.navitype = 'middle'
         if( this.page == this.pages.length - 1 ) footer.dataset.navitype = 'last'
@@ -77,13 +80,12 @@ class Menu{
     constructor(){
         this.node = document.querySelector( '#menu' )
         this.pages = []
-
+        this.active = 'main'
         var pages = this.node.querySelectorAll( '.page' )
         Object.values( pages ).forEach( p => {
             var pageObject
             if( p.dataset.class ) pageObject = new Pages[ p.dataset.class ].default( p )
             else pageObject = new Page( p )
-            // pageObject.on( 'updateFlow', ( e ) => this.update( e ) )
             this.pages.push( pageObject )
         })
 
@@ -93,42 +95,57 @@ class Menu{
                 this.node.querySelector( '.page[data-pid=menu]' ).classList.remove( 'active' )
                 this.node.querySelector( '.page[data-pid=' + e.target.dataset.target + ']' ).classList.remove( 'right' )
                 this.node.querySelector( '.page[data-pid=' + e.target.dataset.target + ']' ).classList.add( 'active' )
-                footer.dataset.navitype = 'inMenu'
-
+                this.active = e.target.dataset.target
                 var headerTitle = document.querySelector( '#pageTitle' )
-                headerTitle.innerHTML = this.node.querySelector( '.page[data-pid=' + e.target.dataset.target + ']' ).dataset.title
+                headerTitle.innerHTML = this.node.querySelector( '.page[data-pid=' + e.target.dataset.target + ']' ).dataset.title 
             } )
         } ) 
     }
 
-    backToHome( ){
-        this.node.querySelector( '.active' ).classList.add( 'right' )
-        this.node.querySelector( '.active' ).classList.remove( 'active' )
-        this.node.querySelector( '.page[data-pid=menu]' ).classList.remove( 'left' )
-        this.node.querySelector( '.page[data-pid=menu]' ).classList.add( 'active' )
-        footer.dataset.navitype = 'none'
+    back( ){
+        if( this.active !== 'main' ) {
+            this.node.querySelector( '.active' ).classList.add( 'right' )
+            this.node.querySelector( '.active' ).classList.remove( 'active' )
+            this.node.querySelector( '.page[data-pid=menu]' ).classList.remove( 'left' )
+            this.node.querySelector( '.page[data-pid=menu]' ).classList.add( 'active' )
+            var headerTitle = document.querySelector( '#pageTitle' )
+            headerTitle.innerHTML = 'Menu'
+            this.active = 'main'
+        } else {
+            this.close()
+        }
+        
+    }
+
+    open(){
+        
+        footer.dataset.navitype = 'inMenu'
+        header.classList.add( 'inMenu' )
+
         var headerTitle = document.querySelector( '#pageTitle' )
+        headerTitle.classList.remove( 'main' )
         headerTitle.innerHTML = 'Menu'
+    }
+
+    close(){
+
+        header.classList.remove( 'inMenu' )
+
+        this.node.classList.remove( 'active' )
+        flow.navigate( flow.page )
+
+        this.pages.forEach( p => {
+            p.node.classList.remove( 'active' ) 
+            p.node.classList.add( 'right' ) 
+        })
+        this.pages[ 0 ].node.classList.add( 'active' )
+        this.pages[ 0 ].node.classList.remove( 'left', 'right' )
     }
 
     toggleActive( ){
         this.node.classList.toggle( 'active' )
-        if( this.node.classList.contains( 'active' ) ){
-            var footer = document.querySelector( '#footer' )
-            footer.dataset.navitype = 'none'
-
-            var header = document.querySelector( '#header' )
-            header.classList.add( 'inMenu' )
-
-            var headerTitle = document.querySelector( '#pageTitle' )
-            headerTitle.classList.remove( 'main' )
-            headerTitle.innerHTML = 'Menu'
-
-        } else {
-            var header = document.querySelector( '#header' )
-            header.classList.remove( 'inMenu' )
-            flow.navigate( flow.page )
-        }
+        if( this.node.classList.contains( 'active' ) ) this.open()
+        else this.close()
     }
 }
 
@@ -138,7 +155,5 @@ var menu = new Menu()
 
 document.querySelector( '.arrow.right' ).addEventListener( 'click', ( ) => flow.navigate( ++flow.page ) )
 document.querySelector( '.arrow.left' ).addEventListener( 'click', ( ) => flow.navigate( --flow.page ) )
-document.querySelector( '.arrow.menu' ).addEventListener( 'click', ( ) => menu.backToHome( ) )
+document.querySelector( '.arrow.menu' ).addEventListener( 'click', ( ) => menu.back( ) )
 document.querySelector( '#menuBut' ).addEventListener( 'click', ( ) => menu.toggleActive() )
-
-// menu.toggleActive()
